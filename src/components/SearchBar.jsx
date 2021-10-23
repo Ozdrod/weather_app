2b91da127460a39f95f144af125f2754c57b3708
Oscar Rodriguez <Rodriguez.oscardaniel@outlook.com>
2021-10-23 15:39:51 -0300

    base styles
    b ase styles plus video bkg attempt

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 import React from 'react';
+=======
+import { func } from "prop-types";
+import React from "react";
+import styles from "./SearchBar.module.css";
+import { IoSearchOutline } from "react-icons/io5";
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
 export default function SearchBar({onSearch}) {
   // acá va tu código
     const handleOnSearch = () => {
@@ -22,3 +30,21 @@ export default function SearchBar({onSearch}) {
     </button>
   </div>
 };
+=======
+  return (
+    <form className={styles.searchBar}>
+      <input
+        id="searchInput"
+        placeholder="Buscar una ciudad..."
+        autoComplete="off"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onSubmit={handleOnSearch}
+      />
+      <button type="submit" onClick={handleOnSearch}>
+        <IoSearchOutline />
+      </button>
+    </form>
+  );
+}
+>>>>>>> Stashed changes
